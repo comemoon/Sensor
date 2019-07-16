4369836ef8550b5b060783ad03772e549637f386
comemoon <531443128@qq.com>
2019-07-16 19:33:54 +0800

    建立了一个布局

--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sensor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"66",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"66",Toast.LENGTH_SHORT).show();
+
+
     }
 }
