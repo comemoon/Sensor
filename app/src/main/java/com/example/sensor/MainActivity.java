4b13404bd2b6b19b3034a8aebd3297b46f00080b
Lenovo <2529636439@qq.com>
2019-07-17 18:05:52 +0800

    布局

--- app/src/main/java/com/example/sensor/MainActivity.java
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -1,29 +1,62 @@
 package com.example.sensor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.sensor.fragments.CallFragment;
+import com.example.sensor.fragments.ChartFragment;
+import com.example.sensor.fragments.PoliceFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    replaceFragment(new PoliceFragment());
+                    return true;
+                case R.id.navigation_dashboard:
+                    replaceFragment(new CallFragment());
+                    return true;
+                case R.id.navigation_notifications:
+                    replaceFragment(new ChartFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"66",Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"66",Toast.LENGTH_SHORT).show();
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(intent);
-            }
-        });
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        replaceFragment(new PoliceFragment());
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment,fragment).commit();
 
     }
 }
---
+++ app/src/main/java/com/example/sensor/star_Activity.java
@@ -0,0 +1,38 @@
+package com.example.sensor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.RelativeLayout;
+
+public class star_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_);
+        RelativeLayout relativeLayout=(RelativeLayout) findViewById(R.id.star);
+        AlphaAnimation alphaAnimation=new AlphaAnimation(0.1f,1.0f);
+        alphaAnimation.setDuration(3000);
+        relativeLayout.startAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent =new Intent(star_Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
